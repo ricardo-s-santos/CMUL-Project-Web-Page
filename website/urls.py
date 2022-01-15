91f21ea6baeb96b7d8b4cfdf1cb20523ae681ad2
Ricardo Santos <a21904332@alunos.ulht.pt>
2022-01-15 22:24:16 +0000

    Adicionadas página modelos negócio e futuro.

--- website/urls.py
+++ website/urls.py
@@ -6,5 +6,12 @@ app_name = "website"
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('gsm/', views.gsm, name='gsm'),
+    path('evolucao/', views.evolucao, name='evolucao'),
+    path('tecnologia/', views.tecnologia, name='tecnologia'),
+    path('bussiness_models/', views.bussiness_models, name='bussiness_models'),
+    path('interfaces/', views.interfaces, name='interfaces'),
+    path('impactos_sociais/', views.impactos_sociais, name='impactos_sociais'),
+    path('impactos_eticos/', views.impactos_eticos, name='impactos_eticos'),
+    path('portugal/', views.portugal, name='portugal'),
+    path('futuro/', views.futuro, name='futuro'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, "website/index.html")
@@ -9,6 +10,33 @@ def about(request):
     return render(request, "website/about.html")
 
 
-def gsm(request):
-    return render(request, "website/gsm.html")
+def evolucao(request):
+    return render(request, "website/evolucao.html")
 
+
+def tecnologia(request):
+    return render(request, "website/tecnologia.html")
+
+
+def bussiness_models(request):
+    return render(request, "website/bussiness_Models.html")
+
+
+def interfaces(request):
+    return render(request, "website/interfaces.html")
+
+
+def impactos_sociais(request):
+    return render(request, "website/impactos_sociais.html")
+
+
+def impactos_eticos(request):
+    return render(request, "website/impactos_eticos.html")
+
+
+def portugal(request):
+    return render(request, "website/portugal.html")
+
+
+def futuro(request):
+    return render(request, "website/futuro.html")
